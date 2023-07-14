e16c960bc8875a0ab1563a27803161c787583a0a
ns2772 <bytelogic772@gmail.com>
2023-07-14 10:47:02 -0400

    task completion

--- src/App.js
+++ src/App.js
@@ -1,25 +1,90 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from 'react';
 
-function App() {
+const fetchData = () => {
+  // Simulating an asynchronous API call to fetch data
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      // Make up a data set to demonstrate the solution
+      const transactions = [
+        { customer: 'John', month: 'January', amount: 120 },
+        { customer: 'John', month: 'February', amount: 80 },
+        { customer: 'John', month: 'March', amount: 150 },
+        { customer: 'Sarah', month: 'January', amount: 60 },
+        { customer: 'Sarah', month: 'February', amount: 110 },
+        { customer: 'Sarah', month: 'March', amount: 90 },
+      ];
+      resolve(transactions);
+    }, 1000);
+  });
+};
+
+const RewardPointsCalculator = () => {
+  const [transactions, setTransactions] = useState([]);
+
+  useEffect(() => {
+    fetchData().then((data) => {
+      setTransactions(data);
+    });
+  }, []);
+
+  const calculateRewardPoints = (amount) => {
+    let rewardPoints = 0;
+
+    if (amount > 100) {
+      rewardPoints += (amount - 100) * 2;
+    }
+
+    if (amount > 50 && amount <= 100) {
+      rewardPoints += amount - 50;
+    }
+
+    return rewardPoints;
+  };
+
+  const calculateTotalRewardPoints = (customer) => {
+    const customerTransactions = transactions.filter(
+      (transaction) => transaction.customer === customer
+    );
+    const totalRewardPoints = customerTransactions.reduce(
+      (total, transaction) => total + calculateRewardPoints(transaction.amount),
+      0
+    );
+    return totalRewardPoints;
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h2>Reward Points Calculator</h2>
+      <table>
+        <thead>
+          <tr>
+            <th>Customer</th>
+            <th>Month</th>
+            <th>Transaction Amount</th>
+            <th>Reward Points</th>
+          </tr>
+        </thead>
+        <tbody>
+          {transactions.map((transaction, index) => (
+            <tr key={index}>
+              <td>{transaction.customer}</td>
+              <td>{transaction.month}</td>
+              <td>${transaction.amount}</td>
+              <td>{calculateRewardPoints(transaction.amount)}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+      <h3>Total Reward Points</h3>
+      {Array.from(new Set(transactions.map((transaction) => transaction.customer))).map(
+        (customer) => (
+          <p key={customer}>
+            {customer}: {calculateTotalRewardPoints(customer)}
+          </p>
+        )
+      )}
     </div>
   );
-}
+};
 
-export default App;
+export default RewardPointsCalculator;
